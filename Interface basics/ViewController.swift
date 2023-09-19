0a349db337a819b14390ed45958f03fa7f28448c
Lenayah <lfanesie@gmail.com>
2023-09-19 14:01:03 -0400

    update

--- Interface basics/ViewController.swift
+++ Interface basics/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var secondField: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func greet(_ sender: UIButton) {
+        secondField.text = "Hello " +  (textField.text ?? "")
+        
+    }
+    
+       
+    
 }
 
